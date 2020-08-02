02d3cb1c199e2142baab32680888a1fb2bc24b2d
Sayak B <sayak-brm@users.noreply.github.com>
2020-08-02 05:59:17 +0530

    Fix OSS pages path naming bug

--- pre/github_readme.py
+++ pre/github_readme.py
@@ -23,9 +23,9 @@ def gen_readme(repo):
     path = repo.name 
     if repo.name in paths.keys():
         path = paths[repo.name]
-    path = f"content/projects/{path}.md"
     if repo.fork:
-        path = f"content/projects/oss/{path}.md"
+        path = f"oss/{path}"
+    path = f"content/projects/{path}.md"
 
     with open(path, 'wb') as readme:
         readme.write(b"---\ntitle: ")
