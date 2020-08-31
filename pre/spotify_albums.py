aada038dfc0c266e9c2d96dc39e5c35d357896b9
Sayak B <sayak-brm@users.noreply.github.com>
2020-09-01 02:40:42 +0530

    Update spotify_albums.py

--- pre/spotify_albums.py
+++ pre/spotify_albums.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import json
+from datetime import datetime
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -15,12 +16,24 @@ def get_albums(artist_uri):
         albums.extend(results['items'])
     return albums
 
+def gen_album(album):
+    date = datetime.strptime(album["release_date"], "%Y-%m-%d")
+    spotify_uri = "spotify:album:" + album["id"]
+    with open(f"content/music/{album['name']}.md", "w") as page:
+        page.write("---\ntitle: ")
+        page.write(album["name"])
+        page.write("\ndate: ")
+        page.write(date.strftime("%Y-%m-%dT%H:%M:%S+05:30"))
+        page.write("\ndraft: false\ntoc: false\ntags: \n")
+        page.write("  - music\n  - sayak b\n  - edm\n")
+        page.write("---\n\n")
+
 
 def main():
     artist_uri = json.load(open("pre/spotify_albums/artist_uri.json"))
     albums = get_albums(artist_uri)
     
     for album in albums:
-        print(album['name'])
+        gen_album(album)
 
 main()
