65f992b4eeb08b186b842a27c55c1892dd169a29
Sayak B <sayak-brm@users.noreply.github.com>
2020-10-23 01:07:18 +0530

    Switch from Patch to Preprocessor

--- pre/theme_mods.py
+++ pre/theme_mods.py
@@ -7,7 +7,11 @@ for mod_fn in mods.keys():
     with open(mod_fn, 'r') as mod_file:
         con = mod_file.read()
     loc = con.find(mods[mod_fn][0])
-    con = con[:loc+mods[mod_fn][2]] + mods[mod_fn][1] + con[loc+len(mods[mod_fn][1]):]
+    if mods[mod_fn][2]:
+        con = con[:loc] + mods[mod_fn][1] + con[loc+len(mods[mod_fn][0]):]
+    else:
+        loc = loc + len(mods[mod_fn][0])
+        con = con[:loc] + mods[mod_fn][1] + con[loc:]
     with open(mod_fn, 'w') as mod_file:
         con = mod_file.write(con)
     print("Modded file:", mod_fn)
